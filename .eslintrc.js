2b6f3e0469f53e1aaa1c3de1eae285724349023a
Chris Ruppel <chris.ruppel@gmail.com>
2018-01-06 16:21:19 +0100

    :construction: allow linting of files

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": "eslint:recommended",
+  "env": {
+    "node": true,
+  },
+  "globals": {
+  },
+  "rules": {
+    "comma-dangle": ["error", "always-multiline"],
+    "no-console": "off",
+    "semi": ["error", "always"],
+  }
+};
